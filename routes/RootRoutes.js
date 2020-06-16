d9036620b9bcbcbb11c34706d81f5bf8ac6bcffc
tkoberle <tomkoberle@gmail.com>
2020-06-16 20:18:36 -0300

    no-op change to test auto image build on Docker Hub

--- routes/RootRoutes.js
+++ routes/RootRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res) => {
-    res.send('Welcome to my bike maintenance application');
+    res.send('Welcome to my New and improved bike maintenance application');
 })
 
 module.exports = router;
