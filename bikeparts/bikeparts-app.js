9ddfd127441a96a623239964b2bab189c24b15aa
tkoberle <tomkoberle@gmail.com>
2020-07-14 19:49:07 -0300

    split bike and bikeparts into 2 apps with individual Dockerfiles

--- bikeparts/bikeparts-app.js
+++ bikeparts/bikeparts-app.js
@@ -2,17 +2,14 @@
 const express = require('express');
 
 // Routes
-const rootRoutes = require('../RootRoutes');
 const bikePartsRouter = require('./BikePartRoutes');
 
 // Middleware
-const logger = require('../middleware/logger');
-
+const logger = require('./logger')
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(logger);
 app.use('/api/bikeparts',bikePartsRouter);
-app.use('/',rootRoutes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- bikes/bikes-app.js
+++ bikes/bikes-app.js
@@ -2,18 +2,15 @@
 const express = require('express');
 
 // Routes
-const rootRoutes = require('../RootRoutes');
-const bikeRouter = require('../bikes/BikeRoutes');
-const bikePartsRouter = require('./BikePartRoutes');
+const bikeRouter = require('./BikeRoutes');
 
 // Middleware
-const logger = require('../middleware/logger');
+const logger = require('./logger');
 
 const port = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(logger);
 app.use('/api/bikes',bikeRouter);
-app.use('/',rootRoutes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
